6776bc85447c04234f7dfd0454d87da99dc78f1f
HydroLancer <36414472+HydroLancer@users.noreply.github.com>
2018-03-21 02:00:34 +0000

    vector movement now working
    issues with countdown at start
    wondering how to spawn dummies on checkpoints... probably obvious, I'm just a dummy dum dum.

--- Assignment 3.cpp
+++ Assignment 3.cpp
@@ -3,31 +3,57 @@
 // by Nathan Hendley			 //
 ///////////////////////////////////
 
-#include <TL-Engine.h>;	// TL-Engine include file and namespace
+#include <TL-Engine.h>	// TL-Engine include file and namespace
+#include <sstream>
+#include <vector>
+using namespace std;
 using namespace tle;
 bool floatingUp = true;
-float Zero = 0.0f;
+float timer;
+
+struct checkpoints
+{
+	float x;
+	float z;
+	float leg = 9.86f;
+};
+
+struct vectors
+{
+	float x;
+	float z;
+};
+
+vectors scalar(float scalar, vectors v)
+{
+	return{ scalar * v.x, scalar * v.z };
+}
+
+vectors sum(vectors v1, vectors v2, vectors v3)
+{
+	return{ v1.x + v2.x + v3.x, v1.z + v2.z + v3.z };
+}
+
+vectors momentum{ 0.0f, 0.0f };
+vectors thrust{ 0.0f, 0.0f };
+vectors drag{ 0.0f, 0.0f };
 
 enum currentState {Countdown, Go, FirstCheckpoint, Finish};
 
+
 I3DEngine* myEngine = New3DEngine(kTLX);
 
-float carFacing;
-float timer;
+float maxSpeed = 0.03f;
 
 EKeyCode forwards = Key_W;
 EKeyCode backwards = Key_S;
 EKeyCode rightTurn = Key_D;
 EKeyCode leftTurn = Key_A;
 
-float currentSpeed = 0.0f;
-float maxSpeed = 0.01f;
-float maxBackSpeed = -0.01f;
 float rotateSpeed = 50.0f;
-float bankSpeed = 0.003f;
 
-float drag = 0.01f;
-float thrustFactor = 0.05f;
+float dragFactor = -0.04f;
+float thrustFactor = 0.02f;
 
 float floatspeed = 0.0002f;
 float currentfloat = 0.0f;
@@ -42,51 +68,56 @@ float wallXSpawn[2] = { -10.5f, 9.5f };
 float isleXSpawn[4] = { 10.0f, -10.0f, 10.0f, -10.0f };
 float isleZSpawn[4] = { 40.0f, 40.0f, 53.0f, 53.0f  };
 
-//Deals with car movement (forwards, backwards, and rotation. Not vector moved yet)
+float checkpointNubX = 9.86f;
+
+float counter = 4.0f;
+
+float carMatrix[4][4];
+//Deals with car movement
 void carMovement(IModel* model)
 {
-	if (myEngine->KeyHeld(forwards))
-	{
-		if (currentSpeed < maxSpeed)
-		{
-			currentSpeed = currentSpeed + thrustFactor * timer;
-		}
-		else
-		{
-			currentSpeed;
-		}
-	}
-	if (myEngine->KeyHeld(backwards))
-	{
-		if (currentSpeed > maxBackSpeed)
-		{
-			currentSpeed = currentSpeed - thrustFactor * timer;
-		}
-		else
-		{
-			currentSpeed;
-		}
-	}
+	model->GetMatrix(&carMatrix[0][0]);
+	vectors facingVector = { carMatrix[2][0], carMatrix[2][2] };
+	
 	if (myEngine->KeyHeld(leftTurn))
 	{
-		model->RotateLocalY(-rotateSpeed * timer);
+		model->RotateLocalY(-rotateSpeed*timer);
 	}
 	if (myEngine->KeyHeld(rightTurn))
 	{
-		model->RotateLocalY(rotateSpeed * timer);
+		model->RotateLocalY(rotateSpeed*timer);
 	}
 
-	if (currentSpeed > 0)
+	if (myEngine->KeyHeld(forwards))
 	{
-		model->MoveLocalZ(currentSpeed);
-		currentSpeed = currentSpeed - (drag * timer);
+		//Makes sure the car just can't endlessly accelerate
+		if (momentum.z > maxSpeed || momentum.z > maxSpeed || momentum.z < -maxSpeed || momentum.x < -maxSpeed)
+		{
+			thrust = { 0.0f, 0.0f };
+		}
+		//otherwise, gotta go fast!
+		else
+		{
+			thrust = scalar(thrustFactor * timer, facingVector);
+		}
 	}
-	else if (currentSpeed < 0)
+	//stops going fast if you let go of W.
+	else
 	{
-		
-		model->MoveLocalZ(currentSpeed);
-		currentSpeed = currentSpeed + (drag * timer);
+		thrust = { 0.0f, 0.0f };
 	}
+	//Beep... Beep... Beep..
+	if (myEngine->KeyHeld(backwards))
+	{
+		thrust = scalar(-thrustFactor * timer, facingVector);
+	}
+
+	//nyoom
+	drag = scalar(dragFactor * timer, momentum);
+	momentum = sum(momentum, thrust, drag);
+
+	model->Move(momentum.x, 0.0f, momentum.z);
+	
 }
 
 //Floaty!
@@ -125,11 +156,17 @@ void main()
 	ICamera* camera = myEngine->CreateCamera(kManual, 0.0f, 10.0f, -20.0f);
 	camera->RotateLocalX(20.0f);
 
+	ISprite* uiElement = myEngine->CreateSprite("testUI.png", 40, 560);
+
 	IMesh* skyboxMesh = myEngine->LoadMesh("Skybox 07.x");
 	IModel* skybox = skyboxMesh->CreateModel(0.0f, -960.0f, 0.0f);
 
 	IMesh* floorMesh = myEngine->LoadMesh("ground.x");
 	IModel* floor = floorMesh->CreateModel(0, 0, 0);
+
+	IMesh* dummyMesh = myEngine->LoadMesh("dummy.x");
+	IModel* cardummy = dummyMesh->CreateModel(0,0,0);
+	vector <IModel*> checkpointdummy;
 
 	IMesh* carMesh = myEngine->LoadMesh("race2.x");
 	IModel* car = carMesh->CreateModel(0.0f, 0.01f, -50.0f);
@@ -149,6 +186,10 @@ void main()
 	for (int i = 0; i < 2; i++)
 	{
 		checkpoint[i] = checkpointMesh->CreateModel(0, 0, checkpointZSpawn[i]);
+		for (int j = 0; j < 2; j++)
+		{
+			//spawn dummies for collision??
+		}
 		wall[i] = wallMesh->CreateModel(wallXSpawn[i], 0, wallZSpawn);
 	}
 	//isles
@@ -156,7 +197,9 @@ void main()
 	{
 		isle[j] = isleMesh->CreateModel(isleXSpawn[j], 0, isleZSpawn[j]);
 	}
-	
+	currentState gameState = Countdown;
+
+	car->AttachToParent(cardummy);
 	camera->AttachToParent(car);
 	timer = myEngine->Timer();
 	// The main game loop, repeat until engine is stopped
@@ -167,10 +210,37 @@ void main()
 
 		/**** Update your scene each frame here ****/
 		timer = myEngine->Timer();
-
-		carFloaty(car);
-		carMovement(car);
 		
+		//CAN'T MAKE IT WORK????
+		/*if (gameState == Countdown)
+		{
+			if (counter <= 4.0f && counter > 3.0f)
+			{
+				myFont->Draw("3..", 150, 50, kRed);
+				counter = counter - myEngine->Timer();
+			}
+			if (counter < 2.9f && counter > 2.0f)
+			{
+				myFont->Draw("2..", 150, 50, kRed );
+				counter = counter - myEngine->Timer();
+			}
+
+			if (counter < 1.9f && counter > 1.0f)
+			{
+				myFont->Draw("1..", 150, 50, kRed);
+				counter = counter - myEngine->Timer();
+			}
+			if (counter < 0.9f && counter > 0.0f)
+			{
+				myFont->Draw("Go!", 150, 50, kGreen);
+				counter = counter - myEngine->Timer();
+			}
+		}*/
+		/*if (gameState == Go)
+		{*/
+			carFloaty(car);
+			carMovement(car);
+		//}
 		
 	}
 
